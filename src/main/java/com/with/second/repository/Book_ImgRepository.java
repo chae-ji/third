8b4b52b7d1c9347d0e4e9160b0934f3847456b0e
ChaeJiwoong <cowldnd2@naver.com>
2021-09-09 15:57:12 +0900

    dd

--- src/main/java/com/with/second/repository/Book_ImgRepository.java
+++
@@ -1,7 +0,0 @@
-package com.with.second.repository;
-
-import com.with.second.entity.Book_ImgEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Book_ImgRepository extends JpaRepository<Book_ImgEntity, Long> {
-}
--- src/main/java/com/with/second/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.with.second.repository;
-
-import com.with.second.entity.OrderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
-}
