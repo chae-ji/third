70f46a0a2818b453c282b7caf8be937fcbafa39f
ChaeJiwoong <cowldnd2@naver.com>
2021-11-22 00:33:14 +0900

    spring

---
+++ springmvc/src/main/java/hello/springmvc/basic/response/Unicode.java
@@ -0,0 +1,9 @@
+package hello.springmvc.basic.response;
+
+public class Unicode {
+
+    public static void main(String[] args) {
+        char ch = '\uae00';
+        System.out.println(ch);
+    }
+}
